78718520273c1f1c7bbfd9a410659bdda6b4ac13
Christian Bräunlich <braeunlichchristian@gmail.com>
2025-09-24 15:29:38 +0200

    add example

---
+++ DE/.AL-Go/NewBcContainer.ps1
@@ -0,0 +1,45 @@
+(
+    [hashtable]$parameters
+)
+
+Write-Host "Restoring database from backup ..."
+
+$databaseServer = 'host.containerhelper.internal'
+$databaseInstance = ''
+$databaseName = 'MYDATABASE'
+$databaseUsername = 'bc-docker-devops'
+$databasePassword = '1234'
+$databaseSecurePassword = ConvertTo-SecureString -String $databasePassword -AsPlainText -Force
+$databaseCredential = New-Object pscredential $databaseUsername, $databaseSecurePassword
+
+$connectionString = "Server=localhost;Database=master;User Id=$databaseUsername;Password=$databasePassword;"
+$queryScript = @"
+    DECLARE @terminate NVARCHAR(MAX) =N'';
+
+    SELECT @terminate = @terminate + 'KILL ' + CAST(session_id AS NVARCHAR(10)) + ';'
+    FROM sys.dm_exec_sessions
+    WHERE database_id = DB_ID(N'$databaseName');
+
+    EXEC(@terminate);
+
+    IF DB_ID(N'$databaseName') IS NOT NULL
+    BEGIN
+        DROP DATABASE [$databaseName];
+    END
+
+    RESTORE DATABASE [$databaseName]
+    FROM DISK = N'C:\ProgramData\BcContainerHelper\temp\mydatabase'
+    WITH REPLACE,
+        MOVE N'Demo Database BC (25-0)_Data' TO N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA\$databaseName.mdf',
+        MOVE N'Demo Database BC (25-0)_Log'  TO N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\Log\$databaseName.ldf';
+
+    ALTER DATABASE [$databaseName] SET RECOVERY SIMPLE, MULTI_USER;
+"@
+
+Invoke-Sqlcmd `
+    -ConnectionString $connectionString `
+    -Query $queryScript
+
+New-BcContainer @parameters
+
+Invoke-ScriptInBcContainer -containerName $parameters.ContainerName -scriptblock { $progressPreference = 'SilentlyContinue' }
