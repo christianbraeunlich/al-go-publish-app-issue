0bdd79be4e4becabb40e572352ddf7678629649a
Christian Bräunlich <braeunlichchristian@gmail.com>
2026-03-24 13:38:52 +0100

    update database credentials in NewBcContainer.ps1

--- DE/.AL-Go/NewBcContainer.ps1
+++ DE/.AL-Go/NewBcContainer.ps1
@@ -7,8 +7,8 @@ Write-Host "Restoring database from backup ..."
 $databaseServer = 'host.containerhelper.internal'
 $databaseInstance = ''
 $databaseName = 'CRONUS'
-$databaseUsername = 'admin'
-$databasePassword = 'P@ssw0rd'
+$databaseUsername = 'bc-docker-devops'
+$databasePassword = '1234'
 $databaseSecurePassword = ConvertTo-SecureString -String $databasePassword -AsPlainText -Force
 $databaseCredential = New-Object pscredential $databaseUsername, $databaseSecurePassword
 
